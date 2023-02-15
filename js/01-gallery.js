f0682c352791cdb08e13e76c783603f4d513f264
Alexander <yusovsky2@gmail.com>
2023-02-15 20:03:01 +0200

    trying to create gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+// const lightEl = basicLightbox.create(
+//   `<img src="${galleryItems[0].original}" alt="${galleryItems[0].description}" width="800" height="600">`
+// );
+// document.body.innerHTML = lightEl;
+// lightEl.show();
+console.log(galleryEl);
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+  )
+  .join("");
+
+galleryEl.innerHTML = markupGallery;
